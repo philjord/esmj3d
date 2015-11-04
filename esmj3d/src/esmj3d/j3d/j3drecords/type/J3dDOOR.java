5ace0ee0022645b1fbfa46f58cd16d542d4c7edc
phil <phil@Thinky>
2015-11-05 00:54:17 +0100

    TES3 doors now pivot open

--- esmj3d/src/esmj3d/j3d/j3drecords/type/J3dDOOR.java
+++ esmj3d/src/esmj3d/j3d/j3drecords/type/J3dDOOR.java
@@ -1,8 +1,12 @@
 package esmj3d.j3d.j3drecords.type;
 
+import javax.media.j3d.Alpha;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
 import javax.vecmath.Color3f;
 
 import nif.NifToJ3d;
+import tools3d.utils.TimedRunnableBehavior;
 import tools3d.utils.scenegraph.Fadable;
 import utils.source.MediaSources;
 import esmj3d.data.shared.records.GenericDOOR;
@@ -11,6 +15,12 @@ import esmj3d.j3d.BethRenderSettings;
 public class J3dDOOR extends J3dRECOType
 {
 	private boolean isOpen = false;
+
+	private Alpha alpha;
+
+	private TimedRunnableBehavior pivotBehavior = new TimedRunnableBehavior(10);
+
+	TransformGroup doorPivot = new TransformGroup();
 
 	public J3dDOOR(GenericDOOR reco, boolean makePhys, MediaSources mediaSources)
 	{
@@ -36,7 +46,20 @@ public class J3dDOOR extends J3dRECOType
 					((Fadable) j3dNiAVObject).setOutline(null);
 			}
 
-			addChild(j3dNiAVObject);
+			//TES3 has no animations for doors, so I'ma just pivot around Y!
+			if (j3dNiAVObject.getJ3dNiControllerManager() == null)
+			{
+				doorPivot.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+				doorPivot.addChild(j3dNiAVObject);
+				addChild(doorPivot);
+
+				doorPivot.addChild(pivotBehavior);
+			}
+			else
+			{
+				addChild(j3dNiAVObject);
+
+			}
 			fireIdle();
 		}
 
@@ -59,20 +82,46 @@ public class J3dDOOR extends J3dRECOType
 	public void toggleOpen()
 	{
 		isOpen = !isOpen;
-		if (j3dNiAVObject.getJ3dNiControllerManager() != null)
-			j3dNiAVObject.getJ3dNiControllerManager().getSequence(isOpen ? "Open" : "Close").fireSequenceOnce();
-		else
-			System.out.println("Odd door with no controller, probably travel door");
+		animateDoor();
 	}
 
 	public void setOpen(boolean isOpen)
 	{
 		this.isOpen = isOpen;
-		j3dNiAVObject.getJ3dNiControllerManager().getSequence(isOpen ? "Open" : "Close").fireSequenceOnce();
+		animateDoor();
 	}
 
 	public boolean isOpen()
 	{
 		return isOpen;
 	}
+
+	/**
+	 * called after open is set
+	 */
+	private void animateDoor()
+	{
+		if (j3dNiAVObject.getJ3dNiControllerManager() != null)
+			j3dNiAVObject.getJ3dNiControllerManager().getSequence(isOpen ? "Open" : "Close").fireSequenceOnce();
+		else
+		{
+			//wow TES3 door have no animation, they look like they just artifically pivot around 
+			alpha = new Alpha(1, 500);
+			alpha.setStartTime(System.currentTimeMillis());
+
+			Runnable callback = new Runnable()
+			{
+				public void run()
+				{
+					Transform3D t = new Transform3D();
+					double a = (Math.PI / 2f) * alpha.value();
+					a = isOpen ? a : (Math.PI / 2f) - a;
+					t.rotY(a);
+					doorPivot.setTransform(t);
+				}
+			};
+			pivotBehavior.start(50, callback);
+		}
+	}
+
 }
