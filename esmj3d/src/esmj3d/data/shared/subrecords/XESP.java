405bf624c5152c95405e54a800501f7191353a22
phil <phil@Thinky>
2015-11-10 13:18:45 +0100

    J3dICellFactory now caches persistents records properly

--- esmj3d/src/esmj3d/data/shared/subrecords/XESP.java
+++ esmj3d/src/esmj3d/data/shared/subrecords/XESP.java
@@ -9,8 +9,9 @@ public class XESP
 	// formid - Parent reference (Object to take enable state from)
 	// uint32 - Flags (records with no flags set have random values in high bits)
 
-	//   0x0001 = Set Enable State Opposite Parent
-	//   0x0002 = Pop In
+	public static int ENABLE_OPPOSITE_FLAG = 0x0001; //   0x0001 = Set Enable State Opposite Parent
+
+	public static int POP_IN_FLAG = 0x0002; //   0x0002 = Pop In
 
 	public int parentId;
 
--- esmj3d/src/esmj3d/j3d/cell/J3dICellFactory.java
+++ esmj3d/src/esmj3d/j3d/cell/J3dICellFactory.java
@@ -1,37 +1,98 @@
 package esmj3d.j3d.cell;
 
+import java.util.HashMap;
+
 import javax.media.j3d.BranchGroup;
 
 import utils.source.MediaSources;
-import esmmanager.common.data.plugin.IMaster;
+import esmmanager.common.data.plugin.PluginGroup;
+import esmmanager.common.data.plugin.PluginRecord;
 import esmmanager.common.data.record.IRecordStore;
+import esmmanager.common.data.record.Record;
+import esmmanager.loader.IESMManager;
 
-public interface J3dICellFactory
+public abstract class J3dICellFactory implements IRecordStore
 {
-	public void setSources(IMaster esmManager2, IRecordStore esmManager22, MediaSources mediaSources);
 
-	public String getMainESMFileName();
+	protected IESMManager esmManager;
 
-	public J3dICELLPersistent makeBGWRLDPersistent(int formId, boolean makePhys);
+	protected MediaSources mediaSources;
 
-	public J3dCELLGeneral makeBGWRLDTemporary(int cellId, boolean makePhys);
+	protected HashMap<Integer, PluginGroup> persistentChildrenGroupByFormId = new HashMap<Integer, PluginGroup>();
 
-	public J3dCELLGeneral makeBGWRLDTemporary(int wrldFormId, int x, int y, boolean makePhys);
+	protected HashMap<Integer, Record> persistentChildrenByFormId = new HashMap<Integer, Record>();
 
-	public J3dCELLGeneral makeBGWRLDDistant(int cellId, boolean makePhys);
+	protected HashMap<Integer, Integer> persistentCellIdByFormId = new HashMap<Integer, Integer>();
 
-	public J3dCELLGeneral makeBGWRLDDistant(int wrldFormId, int x, int y, boolean makePhys);
+	public abstract void setSources(IESMManager iesmManager, MediaSources mediaSources);
 
-	public J3dICELLPersistent makeBGInteriorCELLPersistent(int cellId, boolean makePhys);
+	public abstract String getMainESMFileName();
 
-	public J3dCELLGeneral makeBGInteriorCELLTemporary(int cellId, boolean makePhys);
+	public abstract J3dICELLPersistent makeBGWRLDPersistent(int formId, boolean makePhys);
 
-	public J3dCELLGeneral makeBGInteriorCELLDistant(int cellId, boolean makePhys);
+	public abstract J3dCELLGeneral makeBGWRLDTemporary(int cellId, boolean makePhys);
 
-	public String getLODWorldName(int worldFormId);
+	public abstract J3dCELLGeneral makeBGWRLDTemporary(int wrldFormId, int x, int y, boolean makePhys);
 
-	public BranchGroup makeLODLandscape(int lodX, int lodY, int scale, String lodWorldName);
+	public abstract J3dCELLGeneral makeBGWRLDDistant(int cellId, boolean makePhys);
 
-	public boolean isWRLD(int worldFormId);
+	public abstract J3dCELLGeneral makeBGWRLDDistant(int wrldFormId, int x, int y, boolean makePhys);
+
+	public abstract J3dICELLPersistent makeBGInteriorCELLPersistent(int cellId, boolean makePhys);
+
+	public abstract J3dCELLGeneral makeBGInteriorCELLTemporary(int cellId, boolean makePhys);
+
+	public abstract J3dCELLGeneral makeBGInteriorCELLDistant(int cellId, boolean makePhys);
+
+	public abstract String getLODWorldName(int worldFormId);
+
+	public abstract BranchGroup makeLODLandscape(int lodX, int lodY, int scale, String lodWorldName);
+
+	public abstract boolean isWRLD(int worldFormId);
+
+	protected void cachePersistentChildren(PluginGroup cellChildGroups, int parentId)
+	{
+		if (cellChildGroups != null && cellChildGroups.getRecordList() != null)
+		{
+			for (PluginRecord pgr : cellChildGroups.getRecordList())
+			{
+				PluginGroup pg = (PluginGroup) pgr;
+				if (pg.getGroupType() == PluginGroup.CELL_PERSISTENT)
+				{
+					persistentChildrenGroupByFormId.put(parentId, pg);
+					for (PluginRecord pr : pg.getRecordList())
+					{
+						persistentChildrenByFormId.put(pr.getFormID(), new Record(pr));
+						persistentCellIdByFormId.put(pr.getFormID(), parentId);
+					}
+				}
+			}
+		}
+	}
+
+	public int getCellIdOfPersistentTarget(int formId)
+	{
+		return persistentCellIdByFormId.get(formId);
+	}
+
+	@Override
+	public Record getRecord(int formID)
+	{
+		Record r = esmManager.getRecord(formID);
+
+		if (r != null)
+			return r;
+
+		// other wise try the persistents
+		return persistentChildrenByFormId.get(formID);
+
+	}
+
+	@Override
+	public Record getRecord(String edidId)
+	{
+		// note persistent are not loaded this way
+		return esmManager.getRecord(edidId);
+	}
 
 }
