ea453a6f0c83ec3e287f489dca2f4b9a3d96753e
phil <phil@Thinky>
2015-11-12 17:23:19 +0100

    lights improved a bit

--- esmj3d/src/esmj3d/j3d/j3drecords/type/J3dLIGH.java
+++ esmj3d/src/esmj3d/j3d/j3drecords/type/J3dLIGH.java
@@ -3,6 +3,7 @@ package esmj3d.j3d.j3drecords.type;
 import javax.media.j3d.BoundingLeaf;
 import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.Light;
+import javax.media.j3d.Node;
 import javax.media.j3d.PointLight;
 import javax.media.j3d.SpotLight;
 import javax.vecmath.Color3f;
@@ -11,7 +12,12 @@ import javax.vecmath.Point3f;
 import javax.vecmath.Vector3f;
 
 import nif.NifJ3dHavokRoot;
+import nif.NifJ3dVisRoot;
 import nif.NifToJ3d;
+import nif.j3d.J3dNiAVObject;
+import nif.j3d.NiToJ3dData;
+import nif.niobject.NiAVObject;
+import nif.niobject.NiNode;
 import utils.ESConfig;
 import utils.source.MediaSources;
 import esmj3d.data.shared.records.CommonLIGH;
@@ -26,6 +32,8 @@ public class J3dLIGH extends J3dRECOType
 	public J3dLIGH(CommonLIGH ligh, boolean makePhys, MediaSources mediaSources)
 	{
 		super(ligh, ligh.MODL == null ? "" : ligh.MODL.model.str);
+
+		Point3f lightPosition = new Point3f(0, 0, 0);
 		if (ligh.MODL != null)
 		{
 			String nifFileName = ligh.MODL.model.str;
@@ -41,8 +49,13 @@ public class J3dLIGH extends J3dRECOType
 			{
 				if (nifFileName.length() > 0)
 				{
-					j3dNiAVObject = NifToJ3d.loadShapes(nifFileName, mediaSources.getMeshSource(), mediaSources.getTextureSource())
-							.getVisualRoot();
+					NifJ3dVisRoot vr = NifToJ3d.loadShapes(nifFileName, mediaSources.getMeshSource(), mediaSources.getTextureSource());
+					j3dNiAVObject = vr.getVisualRoot();
+
+					// let's find out where the attach point is
+					Vector3f attachNode = findAttachLight(j3dNiAVObject.getNiAVObject(), vr.getNiToJ3dData());
+					if (attachNode != null)
+						lightPosition = new Point3f(attachNode);
 				}
 			}
 
@@ -51,22 +64,22 @@ public class J3dLIGH extends J3dRECOType
 				addChild(j3dNiAVObject);
 				fireIdle();
 			}
+
 		}
 
-		if (BethRenderSettings.isEnablePlacedLights())
+		if (!makePhys && BethRenderSettings.isEnablePlacedLights())
 		{
-
 			Color3f color = new Color3f(ligh.color.x / 255f, ligh.color.y / 255f, ligh.color.z / 255f);
 			System.out.println("new light " + color);
 			System.out.println("falls fade " + ligh.fade + " falloffExponent " + ligh.falloffExponent + " fieldOfView " + ligh.fieldOfView);
 			System.out.println("ligh.radius " + ligh.radius + " " + (ligh.radius * ESConfig.ES_TO_METERS_SCALE));
-			if (ligh.fieldOfView == -1)
+			if (ligh.fieldOfView == -1 || ligh.fieldOfView >= 90f)
 			{
-				light = new PointLight(true, color, new Point3f(0, 0, 0), new Point3f(1, ligh.fade, ligh.falloffExponent));
+				light = new PointLight(true, color, lightPosition, new Point3f(1, ligh.fade, ligh.falloffExponent));
 			}
 			else
 			{
-				light = new SpotLight(true, color, new Point3f(0, 0, 0), new Point3f(1, ligh.falloffExponent, 0), new Vector3f(0, 0, -1),
+				light = new SpotLight(true, color, lightPosition, new Point3f(1, ligh.fade, ligh.falloffExponent), new Vector3f(0, 0, -1),
 						ligh.fieldOfView, 0);
 			}
 			light.setCapability(Light.ALLOW_INFLUENCING_BOUNDS_WRITE);
@@ -76,6 +89,33 @@ public class J3dLIGH extends J3dRECOType
 			addChild(light);
 		}
 
+	}
+
+	public Vector3f findAttachLight(NiAVObject niAVObject, NiToJ3dData niToJ3dData)
+	{
+		//TODO: this should be the classic multiply up the chain gear
+		if (niAVObject.name.equals("AttachLight"))
+		{
+			return new Vector3f(niAVObject.translation.x * ESConfig.ES_TO_METERS_SCALE, niAVObject.translation.z
+					* ESConfig.ES_TO_METERS_SCALE, -niAVObject.translation.y * ESConfig.ES_TO_METERS_SCALE);
+		}
+
+		if (niAVObject instanceof NiNode)
+		{
+			NiNode niNode = (NiNode) niAVObject;
+			for (int i = 0; i < niNode.numChildren; i++)
+			{
+				NiNode childNode = (NiNode) niToJ3dData.get(niNode.children[i]);
+				if (childNode != null)
+				{
+					Vector3f v = findAttachLight(childNode, niToJ3dData);
+					if (v != null)
+						return v;
+				}
+			}
+		}
+
+		return null;
 	}
 
 	public void renderSettingsUpdated()
