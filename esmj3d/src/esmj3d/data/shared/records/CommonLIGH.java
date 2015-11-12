a6e2906cd8e7a32d1f64baa3de2ff87c1e180e8f
phil <phil@Thinky>
2015-11-12 16:46:38 +0100

    CommonLIGH made abstract

--- esmj3d/src/esmj3d/data/shared/records/CommonLIGH.java
+++ esmj3d/src/esmj3d/data/shared/records/CommonLIGH.java
@@ -5,17 +5,17 @@ import javax.vecmath.Color3f;
 import esmj3d.data.shared.subrecords.MODL;
 import esmmanager.common.data.record.Record;
 
-public class CommonLIGH extends RECO
+public abstract class CommonLIGH extends RECO
 {
-	
+
 	public MODL MODL = null;
-	
-	public Color3f color;
+
+	public Color3f color = new Color3f(255, 255, 255);// white by esm system of 255
 
 	public float radius;
-	
+
 	public float fade = 0;
-	
+
 	public float falloffExponent = 0;
 
 	public float fieldOfView = -1f;
